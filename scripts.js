ce3c46b810f56f65dcbb707f110b49a49cff53a1
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2022-02-01 13:26:24 -0500

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var finalDate = new Date ("March 12, 2022 00:00:00").getTime()
+
+setInterval(function(){
+    var deltaTime = finalDate - Date.now() //ms elapsed since the start
+    var days = Math.floor(deltaTime / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((deltaTime % (1000 * 60)) / 1000);
+    
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s"
+
+    if (distance < 0){
+        clearInterval(x)
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000)
+
